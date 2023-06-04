00c5c90f25d2a2a30049225b46bdf06f0deb9bde
Nitinaip <nitinai.kan@gmail.com>
2023-06-04 14:49:55 +0700

    .

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,29 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Module1Service } from '../shared/service/module1.service';
 
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
-  styleUrls: ['./page2.component.scss']
+  styleUrls: ['./page2.component.scss'],
 })
 export class Page2Component implements OnInit {
-  public _router!:Router
-  public _route$!:ActivatedRoute
-  public selectedId!:number
+  public _router!: Router;
+  public _route$!: ActivatedRoute;
+  public selectedId!: number;
+  public result!: any;
   constructor(
-  private router:Router,
-  private route: ActivatedRoute
-  ){
-    this._route$ = route
+    private router: Router,
+    private route: ActivatedRoute,
+    private module1Service: Module1Service
+  ) {
+    this._route$ = route;
   }
   ngOnInit(): void {
-    
-    this._route$.paramMap.subscribe(param => {
+    this._route$.paramMap.subscribe((param) => {
       this.selectedId = Number(param.get(`id`));
-    })
-    this._route$.queryParams.subscribe(param => {
-      console.log(param)
+    });
+    this._route$.queryParams.subscribe((param) => {
+      console.log(param);
+    });
+    this.module1Service.get().subscribe({
+      next:(nexxt) => {
+        console.log(nexxt)
+        this.result = nexxt
+      },
+      error: err => {
+        console.log(err)
+      }
     })
   }
-
 }
--- src/app/shared/service/module1.service.ts
+++ src/app/shared/service/module1.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@env'
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,9 @@ export class Module1Service {
   constructor(
     private http:HttpClient
   ) { }
+
+  get(){
+    return this.http.post(`${environment.apiUrl}/api/v1/masterdata/getall`,{})
+  }
   
 }
